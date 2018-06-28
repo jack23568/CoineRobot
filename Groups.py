0eb58fab9f6b6a7c6cec55810e1ce4768b08972d
jack23568 <Jerry@email.com>
2018-06-28 22:56:08 +0800

    第一次提交

---
+++ Groups.py
@@ -0,0 +1,14 @@
+# coding:utf-8 
+# @Time    : 2018/6/26 11:49
+# @Author  : cjr
+# @Email   : 2352404393@qq.com
+# @File    : Groups.py
+# @Software: PyCharm
+
+
+class Group(object):
+
+    def __init__(self):
+        self.group_id = 0
+        self.group_name = ""
+        self.number = 0
